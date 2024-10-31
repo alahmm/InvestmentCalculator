fda9700994de2d8afe1928351e26c432c3ee7962
Meriem Al Ahmadi Hammou <Meriem.Al.Ahmadi.Hammou@msg.group>
2024-10-31 13:25:47 +0100

    Format the results

--- src/app/investment-results/investment-results.component.ts
+++ src/app/investment-results/investment-results.component.ts
@@ -1,10 +1,13 @@
 import {Component, Input} from '@angular/core';
 import {InvestmentResult} from "../investmentg-result.model";
+import {CurrencyPipe} from "@angular/common";
 
 @Component({
   selector: 'app-investment-results',
   standalone: true,
-  imports: [],
+  imports: [
+    CurrencyPipe
+  ],
   templateUrl: './investment-results.component.html',
   styleUrl: './investment-results.component.css'
 })
